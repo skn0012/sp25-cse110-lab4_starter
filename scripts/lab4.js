54ec1072f4bcd224c802ee9d8c50a29bc217d713
skn0012 <122576334+skn0012@users.noreply.github.com>
2025-04-25 18:02:24 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,9 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return false;
+    }
     if (add) {
         const result = 0;
 
@@ -25,6 +28,9 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || discount > 1 || discount < 0 || ) {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
